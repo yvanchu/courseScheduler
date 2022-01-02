2444fdc130bf1933d1b2928221b4a3254e60f1e4
yvanchu <60163603+yvanchu@users.noreply.github.com>
2022-01-02 17:12:58 -0600

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
-const schedule = {
-  title: "CS Courses for 2018-2019",
-  courses: {
-    F101: {
-      id: "F101",
-      meets: "MWF 11:00-11:50",
-      title: "Computer Science: Concepts, Philosophy, and Connections",
-    },
-    F110: {
-      id: "F110",
-      meets: "MWF 10:00-10:50",
-      title: "Intro Programming for non-majors",
-    },
-    S313: {
-      id: "S313",
-      meets: "TuTh 15:30-16:50",
-      title: "Tangible Interaction Design and Learning",
-    },
-    S314: {
-      id: "S314",
-      meets: "TuTh 9:30-10:50",
-      title: "Tech & Human Interaction",
-    },
-  },
-};
 
 const Banner = ({ title }) => <h1>{title}</h1>;
 
@@ -55,6 +29,22 @@ const CourseList = ({ courses }) => (
 );
 
 const App = () => {
+  const [schedule, setSchedule] = useState();
+
+  const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    };
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>loading schedule...</h1>;
+
   return (
     <div className="container">
       <Banner title={schedule.title} />
