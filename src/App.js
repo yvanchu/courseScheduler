58d37e8c8cdd7891323a92a7db23f15911b5966d
yvanchu <60163603+yvanchu@users.noreply.github.com>
2022-01-04 08:24:17 -0600

    Filter by term

--- src/App.js
+++ src/App.js
@@ -20,13 +20,53 @@ const Course = ({ course }) => (
   </div>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-    {Object.values(courses).map((course) => (
-      <Course course={course} />
-    ))}
-  </div>
+const TermButton = ({ term, setTerm, checked }) => (
+  <>
+    <input
+      type="radio"
+      id={term}
+      className="btn-check"
+      autoComplete="off"
+      checked={checked}
+      onChange={() => setTerm(term)}
+    />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+      {term}
+    </label>
+  </>
 );
+
+const TermSelector = ({ term, setTerm }) => {
+  return (
+    <div className="btn-group">
+      {Object.values(terms).map((value) => (
+        <TermButton
+          key={value}
+          term={value}
+          checked={value === term}
+          setTerm={setTerm}
+        />
+      ))}
+    </div>
+  );
+};
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState("Fall");
+  const termCourses = Object.values(courses).filter(
+    (course) => term === getCourseTerm(course)
+  );
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+        {termCourses.map((course) => (
+          <Course course={course} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState();
