5f23932b46d7e80c3509c7f2f5384d698184d63e
yvanchu <60163603+yvanchu@users.noreply.github.com>
2022-01-05 09:51:01 -0600

    Filter by time conflicts

--- src/App.js
+++ src/App.js
@@ -9,16 +9,54 @@ const getCourseTerm = (course) => terms[course.id.charAt(0)];
 
 const getCourseNumber = (course) => course.id.slice(1, 4);
 
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">
-        {getCourseTerm(course)} CS {getCourseNumber(course)}
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const toggle = (x, lst) =>
+    lst.includes(x) ? lst.filter((y) => y !== x) : [x, ...lst];
+
+  const days = ["M", "Tu", "W", "Th", "F"];
+
+  const daysOverlap = (days1, days2) =>
+    days.some((day) => days1.includes(day) && days2.includes(day));
+
+  const hoursOverlap = (hours1, hours2) =>
+    Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end);
+
+  const timeConflict = (course1, course2) =>
+    daysOverlap(course1.days, course2.days) &&
+    hoursOverlap(course1.hours, course2.hours);
+
+  const courseConflict = (course1, course2) =>
+    getCourseTerm(course1) === getCourseTerm(course2) &&
+    timeConflict(course1, course2);
+
+  const hasConflict = (course, selected) =>
+    selected.some((selection) => courseConflict(course, selection));
+
+  const isDisabled = !isSelected && hasConflict(course, selected);
+
+  const style = {
+    backgroundColor: isDisabled
+      ? "lightgrey"
+      : isSelected
+      ? "#B0E5A4"
+      : "white",
+  };
+  return (
+    <div
+      className="card m-1 p-2"
+      style={style}
+      onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}
+    >
+      <div className="card-body">
+        <div className="card-title">
+          {getCourseTerm(course)} CS {getCourseNumber(course)}
+        </div>
+        <div className="card-text">{course.title}</div>
       </div>
-      <div className="card-text">{course.title}</div>
     </div>
-  </div>
-);
+  );
+};
 
 const TermButton = ({ term, setTerm, checked }) => (
   <>
@@ -30,7 +68,7 @@ const TermButton = ({ term, setTerm, checked }) => (
       checked={checked}
       onChange={() => setTerm(term)}
     />
-    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    <label className="btn btn-success m-1 p-2" htmlFor={term}>
       {term}
     </label>
   </>
@@ -53,6 +91,7 @@ const TermSelector = ({ term, setTerm }) => {
 
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState("Fall");
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(
     (course) => term === getCourseTerm(course)
   );
@@ -61,16 +100,50 @@ const CourseList = ({ courses }) => {
       <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
         {termCourses.map((course) => (
-          <Course course={course} />
+          <Course
+            course={course}
+            selected={selected}
+            setSelected={setSelected}
+          />
         ))}
       </div>
     </>
   );
 };
 
+const meetsPat =
+  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = (meets) => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match
+    ? {}
+    : {
+        days,
+        hours: {
+          start: hh1 * 60 + mm1 * 1,
+          end: hh2 * 60 + mm2 * 1,
+        },
+      };
+};
+
+const mapValues = (fn, obj) =>
+  Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, fn(value)])
+  );
+
+const addCourseTimes = (course) => ({
+  ...course,
+  ...timeParts(course.meets),
+});
+
+const addScheduleTimes = (schedule) => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses),
+});
+
 const App = () => {
   const [schedule, setSchedule] = useState();
-
   const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
 
   useEffect(() => {
@@ -78,7 +151,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     };
     fetchSchedule();
   }, []);
