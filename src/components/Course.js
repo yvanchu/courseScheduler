baa33ef88dcb2b06318e31e5d49f7cc88e277c88
yvanchu <60163603+yvanchu@users.noreply.github.com>
2022-01-08 15:20:30 -0600

    Store data

--- src/components/Course.js
+++ src/components/Course.js
@@ -4,6 +4,41 @@ import {
   getCourseTerm,
   hasConflict,
 } from "../utilities/times";
+import { setData } from "../utilities/firebase";
+
+const meetsPat =
+  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = (meets) => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match
+    ? {}
+    : {
+        days,
+        hours: {
+          start: hh1 * 60 + mm1 * 1,
+          end: hh2 * 60 + mm2 * 1,
+        },
+      };
+};
+
+const getCourseMeetingData = (course) => {
+  const meets = prompt("Enter meeting data: MTuWThF hh:mm-hh:mm", course.meets);
+  const valid = !meets || timeParts(meets).days;
+  if (valid) return meets;
+  alert("Invalid meeting data");
+  return null;
+};
+
+const reschedule = async (course, meets) => {
+  if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+    try {
+      await setData(`/courses/${course.id}/meets`, meets);
+    } catch (error) {
+      alert(error);
+    }
+  }
+};
 
 const Course = ({ course, selected, setSelected }) => {
   const isSelected = selected.includes(course);
@@ -24,6 +59,7 @@ const Course = ({ course, selected, setSelected }) => {
       className="card m-1 p-2"
       style={style}
       onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}
+      onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}
     >
       <div className="card-body">
         <div className="card-title">
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -23,6 +23,8 @@ const firebaseConfig = {
 const firebase = initializeApp(firebaseConfig);
 const database = getDatabase(firebase);
 
+export const setData = (path, value) => set(ref(database, path), value);
+
 export const useData = (path, transform) => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
